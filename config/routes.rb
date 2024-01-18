1ca0b4dbfcf0cc75e288986ba3b0c6cf60f18bd7
西亮祐 <nishi.ryosuke@meta-x.co.jp>
2024-01-18 09:54:24 +0900

    error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :events do
-    resources :tickets
-  end
-  resource :retirement
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  resource :retirement, only: %i[new create]
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
+  end
   get "/status" => "status#index", defaults: {format: "json"}
+  match "*path" => "application#error404", via: :all
 end
